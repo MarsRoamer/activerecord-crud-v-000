78beb5aa209d088c7022c9d904c37ccccbaf3c2f
Jonathan Tiede <skysoldiermars@gmail.com>
2018-04-10 18:02:16 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -64,7 +64,8 @@ end
 def can_update_using_update_method
   # Update movie title to "Wat, huh?"
   Movie.create(title: "Wat?")
-  __
+  test = Movie.find_by title: "Wat?"
+  test.update(title: "Wath, huh?")
   __
 end
 
